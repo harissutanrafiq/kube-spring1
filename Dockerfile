bf01e06f7d1945e72962a7e1a65c36c01365b20c
harissutanrafiq <haris.srafiq@gmail.com>
2019-04-10 15:04:40 +0700

    nambah curl di pods

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,8 @@
 FROM openjdk:8-jdk-alpine
+
+RUN apk add --update \
+    curl
+
 MAINTAINER Haris Rafiq <haris.srafiq@gmail.com>
 ADD target/*.jar alfacart-0.0.1-SNAPSHOT.jar
 
--- src/main/java/com/example/alfacart/Tes.java
+++ src/main/java/com/example/alfacart/Tes.java
@@ -18,7 +18,7 @@ public class Tes {
             String namaserver   = InetAddress.getLocalHost().getHostName();
             String ipserver     = InetAddress.getLocalHost().getHostAddress();
 
-            out=  "<h2>hello world AWS EKS server julius</h2>IP : "+ ipserver + "<br> Hostname :" + namaserver  ;
+            out=  "<h2>hello world AWS EKS server nambah curl di pods</h2>IP : "+ ipserver + "<br> Hostname :" + namaserver  ;
             out=out + "<div style='padding:10px'>Halooo</div><br><div>Env:</div>";
             out=out + "<div>SPRING_BEFORE_OPTS: "+System.getenv("SPRING_BEFORE_OPTS")+"</div>";
             out=out + "<div>SPRING_AFTER_OPTS: "+System.getenv("SPRING_AFTER_OPTS")+"</div>";
